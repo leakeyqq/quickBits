c2cda756297926a5831a0aa25c006e049ecd2047
leakeyqq <leakeymugendi3@gmail.com>
2024-03-26 11:51:26 +0300

    User can send to an already existing account

--- controllers/send-transfers.js
+++ controllers/send-transfers.js
@@ -1,18 +1,40 @@
 const mongoose = require('mongoose')
 const SendTransfer = require('./../models/send-transfers')
 const UserWallet = require('./../models/user-wallet')
+const User = require('./../models/user')
 
 const sendForm = async(req,res)=>{
     res.render('send/fill')
 }
-const sendTransfer = async(req,res)=>{
+const transferAsset = async(req,res)=>{
+    try {
+        // INPUTS
+        const senderID = req.user.id
+        const senderEmail = req.user.email
+        const receiverEmail = req.body.receiverEmail
+        const tickerSymbol = req.body.asset
+        const amount_to_send = Number(req.body.amount)
     
-    const senderEmail = req.user.email
-    const receiverEmail = req.body.receiverEmail
+    
+        // VERIFY BALANCE IS SUFFICIENT
+        const userWallet = await UserWallet.findOne({userID: senderID})
+        const tokenBalance = userWallet.balance[tickerSymbol]
+        if(tokenBalance < amount_to_send) throw new Error('Insufficient balance')
 
-    await confirm_send_accounts_exists(senderEmail, receiverEmail)
-    await register_new_transaction()
+    
+        await confirm_send_accounts_exists(senderEmail, receiverEmail)
+        const userWallet_exists = await check_receiver_userWallet_exists(receiverEmail)
+        await register_new_transaction(senderEmail, receiverEmail, tickerSymbol, amount_to_send, userWallet_exists)
+        if(userWallet_exists){
+            await update_userWallet_of_receiver(receiverEmail, tickerSymbol, amount_to_send)
+        }
+        await update_sender_userWallet(senderID, tickerSymbol, amount_to_send)
 
+        res.redirect('/crypto-wallet/balance')
+       
+    } catch (error) {
+        console.error(error)
+    }
 
 }
 const getTokenBalance = async(req,res)=>{
@@ -30,7 +52,7 @@ const getTokenBalance = async(req,res)=>{
 }
 async function confirm_send_accounts_exists(senderEmail, receiverEmail){
     // Check if sender exists in send_transfers records
-    let senderAccount = await SendTransfer({userEmail: senderEmail})
+    let senderAccount = await SendTransfer.findOne({userEmail: senderEmail})
     if(!senderAccount){
         // initialize
         senderAccount = new SendTransfer({
@@ -39,7 +61,7 @@ async function confirm_send_accounts_exists(senderEmail, receiverEmail){
         await senderAccount.save()
     }
     // Check if receiver exists in send_transfers records
-    let receiverAccount = await SendTransfer({userEmail: receiverEmail})
+    let receiverAccount = await SendTransfer.findOne({userEmail: receiverEmail})
     if(!receiverAccount){
         // initialize
         receiverAccount = new SendTransfer({
@@ -48,5 +70,72 @@ async function confirm_send_accounts_exists(senderEmail, receiverEmail){
         await receiverAccount.save()
     }
 }
+async function check_receiver_userWallet_exists(receiver_email){
+    const user = await User.findOne({email: receiver_email})
+    if(user == null){
+        return false
+    }else{
+        return true
+    }
+}
+async function register_new_transaction(sender_email, receiver_email, tickerSymbol, amount, userWallet_exists){
+    const sendTransaction = {
+        tickerSymbol: tickerSymbol,
+        amount: amount,
+        receiver_email: receiver_email,
+        eventTime: new Date(),
+        claimed: userWallet_exists
+    }
 
-module.exports = { sendForm, getTokenBalance }
+    const receiveTransaction = {
+        tickerSymbol: tickerSymbol,
+        amount: amount,
+        sender_email: sender_email,
+        eventTime: new Date(),
+        claimed: userWallet_exists
+    }
+    try {
+        // Save the sender transaction
+        await SendTransfer.updateOne({userEmail: sender_email},
+            {
+                $push: {sent: sendTransaction}
+            })
+
+        // Save the receiver transaction
+        await SendTransfer.updateOne({userEmail: receiver_email},
+            {
+                $push: {received: receiveTransaction}
+            })
+
+    } catch (error) {
+        console.error(error)
+    }
+}
+async function update_userWallet_of_receiver(receiverEmail, tickerSymbol, amount_to_send){
+    const user = await User.findOne({email: receiverEmail})
+    const userID = user.id
+
+    const updateObject = {}
+    updateObject[`balance.${tickerSymbol}`] = amount_to_send
+
+
+    await UserWallet.updateOne({userID: userID},
+        {
+            $inc: updateObject
+        })
+}
+async function update_sender_userWallet(senderID, tickerSymbol, amount_to_send){
+    const updateObject = {}
+    updateObject[`balance.${tickerSymbol}`] =  -amount_to_send
+
+    try {
+        await UserWallet.updateOne({userID: senderID},
+            {
+                $inc: updateObject
+            })     
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+module.exports = { sendForm, getTokenBalance, transferAsset }
--- routes/send-transfer.js
+++ routes/send-transfer.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = express.Router()
+const bodyParser = require('body-parser')
+const urlencodedParser = bodyParser.urlencoded({extended: false})
 
 const { isLoggedIn, isLoggedOut } = require('./../controllers/login-status')
-const { sendForm, getTokenBalance } = require('./../controllers/send-transfers')
+const { sendForm, getTokenBalance, transferAsset } = require('./../controllers/send-transfers')
 
 router.get('/', isLoggedIn , sendForm)
-router.post('/')
+router.post('/', isLoggedIn , urlencodedParser, transferAsset )
 router.get('/token-balance', isLoggedIn ,getTokenBalance)
 module.exports = router
