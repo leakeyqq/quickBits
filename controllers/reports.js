685fd39b07eff067122b78e2caec1da8ec30a44d
leakeyqq <leakeymugendi3@gmail.com>
2024-04-29 00:01:57 +0300

    created reports module

--- controllers/reports.js
+++ controllers/reports.js
@@ -1,6 +1,46 @@
+const mongoose = require('mongoose')
+const UserWallet = require('./../models/user-wallet')
+
 
 const reportForm = (req,res)=>{
     res.render('reports/generate-report')
 }
-module.exports = { reportForm }
+const generate = async(req,res)=>{
+
+    const userID = req.user.id
+
+    const dateRange = {
+        startDate: new Date(req.body.startDate),
+        endDate: new Date(req.body.endDate)
+    }
+
+    const includeBTC = req.body.btc_transactions === 'true';
+    const includeBNB = req.body.bnb_transactions === 'true';
+    const includeUSDT = req.body.usdt_transactions === 'true';
+
+
+    const transaction_type = req.body.transactionType
+
+    if(transaction_type == 'deposits'){
+        let userWallet = await UserWallet.findOne({userID: userID}).lean().exec()
+
+        // Filter deposits within the date range
+        let deposits = userWallet.deposits.filter(deposit => {
+            // return deposit.timeOccurred >= dateRange.startDate && deposit.timeOccurred <= dateRange.endDate
+                // Check if the deposit is within the date range
+        const withinDateRange = deposit.timeOccurred >= dateRange.startDate && deposit.timeOccurred <= dateRange.endDate
+
+        const isBTC = deposit.tickerSymbol === 'BTC'
+        const isBNB = deposit.tickerSymbol === 'BNB'
+        const isUSDT = deposit.tickerSymbol === 'USDT'
+
+        return withinDateRange && ((includeBTC && isBTC) || (includeBNB && isBNB) || (includeUSDT && isUSDT));
+        })
+        console.log(deposits)
+    }
+
+    res.status(200).send('OK')
+
+}
+module.exports = { reportForm, generate }
 
--- routes/reports.js
+++ routes/reports.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const bodyParser = require('body-parser')
+const urlencodedParser = bodyParser.urlencoded({extended: false})
 
-const { reportForm} = require('./../controllers/reports')
+const { reportForm, generate} = require('./../controllers/reports')
+const { isLoggedIn } = require('./../controllers/login-status')
+
+
 router.get('/generate', reportForm)
+router.post('/generate', isLoggedIn, urlencodedParser, generate)
 
 module.exports = router
