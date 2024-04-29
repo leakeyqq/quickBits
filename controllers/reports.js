6691d0049d39de392ea6cf63a186491d9037a266
leakeyqq <leakeymugendi3@gmail.com>
2024-04-29 15:58:19 +0300

    added reports templates

--- controllers/reports.js
+++ controllers/reports.js
@@ -1,15 +1,20 @@
 const mongoose = require('mongoose')
 const UserWallet = require('./../models/user-wallet')
+const SendTransfer = require('./../models/send-transfers')
 
 const pdf = require('pdf-creator-node')
 const fs = require('fs')
+
 
 const options = {
     format: "A4",
     orientation: "portrait",
     border: "10mm"
 }
-const template = fs.readFileSync("./template/template.html", "utf-8")
+const template = fs.readFileSync("./template/deposits-template.html", "utf-8")
+const withdrawalsTemplate = fs.readFileSync("./template/withdrawals-template.html", "utf-8")
+const sentTemplate = fs.readFileSync("./template/sent-template.html", "utf-8")
+const receivedTemplate = fs.readFileSync("./template/received-template.html", "utf-8")
 
 const reportForm = (req,res)=>{
     res.render('reports/generate-report')
@@ -17,11 +22,13 @@ const reportForm = (req,res)=>{
 const generate = async(req,res)=>{
 
     const userID = req.user.id
+    const userEmail = req.user.email
 
     const dateRange = {
         startDate: new Date(req.body.startDate),
         endDate: new Date(req.body.endDate)
     }
+    console.log('inputs are ', req.body)
 
     const includeBTC = req.body.btc_transactions === 'true';
     const includeBNB = req.body.bnb_transactions === 'true';
@@ -35,7 +42,6 @@ const generate = async(req,res)=>{
 
         // Filter deposits within the date range
         let deposits = userWallet.deposits.filter(deposit => {
-            // return deposit.timeOccurred >= dateRange.startDate && deposit.timeOccurred <= dateRange.endDate
                 // Check if the deposit is within the date range
         const withinDateRange = deposit.timeOccurred >= dateRange.startDate && deposit.timeOccurred <= dateRange.endDate
 
@@ -64,10 +70,107 @@ const generate = async(req,res)=>{
             console.log(err)
         })
 
-        console.log(deposits)
     }
+    if(transaction_type == 'withdrawals'){
+        let userWallet = await UserWallet.findOne({userID: userID}).lean().exec()
 
-    // res.status(200).send('OK')
+        // Filter deposits within the date range
+        let withdrawals = userWallet.withdrawals.filter(withdrawal => {
+        const withinDateRange = withdrawal.timeOccurred >= dateRange.startDate && withdrawal.timeOccurred <= dateRange.endDate
+
+        const isBTC = withdrawal.ticker === 'BTC'
+        const isBNB = withdrawal.ticker === 'BNB'
+        const isUSDT = withdrawal.ticker === 'USDT'
+
+        return withinDateRange && ((includeBTC && isBTC) || (includeBNB && isBNB) || (includeUSDT && isUSDT));
+        })
+
+
+        const document = {
+            html: withdrawalsTemplate,
+            data: {
+                withdrawals
+            },
+            path: "./generated-pdfs/withdrawals-report.pdf"
+        }
+
+        pdf.create(document, options).then((fileLocation) => {
+            console.log(fileLocation)
+            const pdfUrl = '/generated-pdfs/withdrawals-report.pdf'
+            res.redirect(pdfUrl)
+
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+
+    }
+    if(transaction_type == 'sent'){
+        let sendTransfer = await SendTransfer.findOne({userEmail: userEmail}).lean().exec()
+
+        // Filter deposits within the date range
+        let sent = sendTransfer.sent.filter(sent_tx => {
+        const withinDateRange = sent_tx.eventTime >= dateRange.startDate && sent_tx.eventTime <= dateRange.endDate
+
+        const isBTC = sent_tx.tickerSymbol === 'btc'
+        const isBNB = sent_tx.tickerSymbol === 'bnb'
+        const isUSDT = sent_tx.tickerSymbol === 'usdt'
+
+        return withinDateRange && ((includeBTC && isBTC) || (includeBNB && isBNB) || (includeUSDT && isUSDT));
+        })
+
+        const document = {
+            html: sentTemplate,
+            data: {
+                sent
+            },
+            path: "./generated-pdfs/sent-report.pdf"
+        }
+
+        pdf.create(document, options).then((fileLocation) => {
+            console.log(fileLocation)
+            const pdfUrl = '/generated-pdfs/sent-report.pdf'
+            res.redirect(pdfUrl)
+
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+
+    }
+    if(transaction_type == 'received'){
+        let sendTransfer = await SendTransfer.findOne({userEmail: userEmail}).lean().exec()
+
+        // Filter deposits within the date range
+        let received = sendTransfer.received.filter(received_tx => {
+        const withinDateRange = received_tx.eventTime >= dateRange.startDate && received_tx.eventTime <= dateRange.endDate
+
+        const isBTC = received_tx.tickerSymbol === 'btc'
+        const isBNB = received_tx.tickerSymbol === 'bnb'
+        const isUSDT = received_tx.tickerSymbol === 'usdt'
+
+        return withinDateRange && ((includeBTC && isBTC) || (includeBNB && isBNB) || (includeUSDT && isUSDT));
+        })
+
+        const document = {
+            html: receivedTemplate,
+            data: {
+                received
+            },
+            path: "./generated-pdfs/received-report.pdf"
+        }
+
+        pdf.create(document, options).then((fileLocation) => {
+            console.log(fileLocation)
+            const pdfUrl = '/generated-pdfs/received-report.pdf'
+            res.redirect(pdfUrl)
+
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+
+    }
 
 }
 module.exports = { reportForm, generate }
